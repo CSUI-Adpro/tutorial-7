0b20c8b319dfb588f4ef4350dc0ee5f908b1a3c5
eugeniusms <eugeniusms@gmail.com>
2023-04-27 19:51:17 +0700

    Update Gradle Kts For Sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "2.7.12-SNAPSHOT"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     id("org.sonarqube") version "3.5.0.2730"
@@ -40,5 +41,21 @@ sonarqube {
         property("sonar.projectKey", "mario-adpro-tutorial-7")
         property("sonar.organization", "mario-adpro")
         property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    classDirectories.setFrom(files(classDirectories.files.map {
+        fileTree(it) { exclude("**/*Application**") }
+    }))
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
     }
 }
